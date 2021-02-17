ff495afe26d92ed562d6b75d648de941433d709a
Alessandro <internicola.alessandro@gmail.com>
2021-02-17 15:59:23 +0100

    Controller Creato
    Creato il controller e le routes per le varie pagine

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+  </head>
+  <style media="screen">
+    *{
+      background-color: blue;
+    }
+  </style>
+  <body>
+    <h1>BLOG</h1>
+  </body>
+</html>
